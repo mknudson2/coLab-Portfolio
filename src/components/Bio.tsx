4a8ebba0a66ae742962d88262c40009509bb290a
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-10-10 11:59:21 -0600

    updated hero section to be text focused and added a gradient glow behind it

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -3,13 +3,13 @@ import { Container } from "react-bootstrap";
 function Bio() {
   return (
     <>
-      <Container>
+      <Container className="bio-container">
         <div>
-          <img src="" alt="" />
+          <img src="/hero-img.JPG" alt="" />
         </div>
         <div>
-          <h3></h3>
-          <p></p>
+          <h3>Michael Knudson</h3>
+          <p>While a Salt Lake City native, I have had the opportunity of living in many different places, including Arizona, Michigan, Wisconsin, Minnesota, Scotland, and Norway. I have loved meeting new people and experiencing new cultures and idea while also recognizing how everything blends and influences one another. <br />In my academic career, this sparked my interest in exploring how different cultures intersect and shape each other. Furthermore, it drove me to find different, unqiue ways to reach my students and make material more understandable or accessible. <br />This same drive has fueled my transition into tech with the joy of using my creativity to make web applications both intuitive to use and accessible regardless of experience.</p>
         </div>
       </Container>
     </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,18 @@
-
 function Hero() {
   return (
     <>
-    <section className="hero-container">
-        <div>
-            <h1 className="hero-header">Where Academia Meets Tech</h1>
-            <h3 className="hero-subtitle">An experienced researcher and educator turned Front-End Dev with a passion to make a user's experience as intuitive and accessible as possible </h3>
+      <section className="hero-container">
+        <div className="hero-bkg">
+          <h1 className="hero-header">Where Academia Meets Tech</h1>
+          <h3 className="hero-subtitle">
+            An experienced researcher and educator turned Front-End Dev with a
+            passion to make a user's experience as intuitive and accessible as
+            possible{" "}
+          </h3>
         </div>
-        <div >
-            <img src="public/hero-img.JPG" alt="Michael and Jamie" className="hero-img"/>
-        </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
