921422bd6ae24158f1929d6bfebbee07e00c8e63
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-10-13 16:37:09 -0600

    created games component with slider.js and the rawg api

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Bio from "./components/Bio";
 import Academic from "./components/Academic";
 import Dev from "./components/Dev";
 import Footer from "./components/Footer";
+import Games from "./components/Games";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
       <div id="dev"></div>
       <hr />
       <Dev />
+      <div id="personal"></div>
+      <hr />
+      <Games />
       <Footer />
     </>
   );
---
+++ src/components/Games.tsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+function Games() {
+  const [games, setGames] = useState<any[]>([]);
+
+  useEffect(() => {
+    const gameSlugs = [
+      "halo-the-master-chief-collection",
+      "middle-earth-shadow-of-war",
+      "hogwarts-legacy",
+      "star-wars-jedi-survivor",
+      "the-legend-of-zelda-breath-of-the-wild",
+      "high-on-life",
+      "assassins-creed-valhalla"
+    ];
+    const baseURL = "https://rawg.io/api/games";
+
+    Promise.all(
+      gameSlugs.map((slug) =>
+        fetch(`${baseURL}/${slug}?key=3805c01271664c5a8cf87bad9405ece5`).then(
+          (response) => response.json()
+        )
+      )
+    )
+      .then((gameData) => {
+        setGames(gameData);
+        console.log(games[1]["background_image"]);
+      })
+      .catch((error) => {
+        console.error("Error fetching game data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="gameContainer">
+        <h2>Games</h2>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 15,
+            stretch: 0,
+            depth:100,
+            modifier: 2.5,
+          }}
+          pagination={{el:'.swiper-pagination', clickable:true}}
+          modules={[EffectCoverflow, Pagination]}
+          className="swiper_container"
+        >
+          <SwiperSlide>
+            <img className="gameImg" src={games[0]["background_image"]} alt="" />
+            <p className="gameText">{games[0]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[1]["background_image"]} alt="" />
+            <p className="gameText">{games[1]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[2]["background_image"]} alt="" />
+            <p className="gameText">{games[2]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[3]["background_image"]} alt="" />
+            <p className="gameText">{games[3]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[4]["background_image"]} alt="" />
+            <p className="gameText">{games[4]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[5]["background_image"]} alt="" />
+            <p className="gameText">{games[5]["name"]}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="gameImg" src={games[6]["background_image"]} alt="" />
+            <p className="gameText">{games[6]["name"]}</p>
+          </SwiperSlide>
+          <br />
+          <div className="slider-controler">
+            <div className="swiper-pagination" id="sliderPosition"></div>
+          </div>
+        </Swiper>
+      </div>
+      <div className="gameParagraph">
+        <p>Games hold a special place in my heart. They have served to inspire me and drive my imagination. As a kid, they brought everyone in our neighborhood together regardless of who they were or if we even ran in the same friend groups. From playing Paper Boy and the original Super Mario Bros with my dad to our weekly game night playing Halo with my dad, brothers, and brother-in-laws, games have been indelibly tied to family time and keeping us together even when we were living in other parts or the world. Because of that, I wanted to showcase a few of my all-time favorites as well some that are currently on my docket. To do this, I used the <strong>rawg.io API</strong> to pull in the various game information, which is the same API I used to create my game hub web app. </p>
+      </div>
+    </>
+  );
+}
+
+export default Games;
