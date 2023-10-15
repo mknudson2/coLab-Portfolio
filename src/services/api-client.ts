3d87974b0de81014a9f0b63075cc8b6c3dca71a7
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-10-15 15:46:16 -0600

    test

--- src/services/api-client.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create ({
-    baseURL: 'https://api.rawg.io/api/games',
-    params: {
-        key: '3805c01271664c5a8cf87bad9405ece5'
-    }
-})
