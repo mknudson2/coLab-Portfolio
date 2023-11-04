b7357a3b694e0a85d0abf73c30ebc5ddd763c870
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-11-04 16:41:18 -0600

    updated github icon, added website icon, and added links to props for dev projects linking to hosted projects

--- src/components/Dev.tsx
+++ src/components/Dev.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { CgWebsite } from "react-icons/cg";
+import { BsGithub } from "react-icons/bs";
 
 function Dev() {
   const [selectedProject, setSelectedProject] = useState<string>("Game Hub");
@@ -9,6 +11,7 @@ function Dev() {
       image: string;
       description: string;
       link: string;
+      web: string;
     }
   > = {
     "Game Hub": {
@@ -16,30 +19,35 @@ function Dev() {
       description:
         "A game discovery web app (Rawg.io clone) that allows the user to filter by genre and platform in addition to searching a specific game. Makes use of the rawg API to get game data. Built with React, Vite, TypeScript, and ChakraUI.",
       link: "https://github.com/mknudson2/game-hub",
+      web: "https://game-hub-eosin-iota.vercel.app/"
     },
     "To-Do List": {
       image: "/ToDo.png",
       description:
         "A simple to-do app that allows the users to add and delete items from a list while assigning priority. Built with JavaScript.",
       link: "https://github.com/mknudson2/week-7-day-3",
+      web: ""
     },
     "Book Collection": {
       image: "/BookCollection.png",
       description:
         "A collection app that allows the user to sign-up/login, create and manage posts, and build a book collection. While the front-end is simple, the back-end is where this project shines along with its own API updated as the users update their collections and posts. Built using Flask, Jinja, PostgreSQL, with API testing via Insomnia.",
       link: "https://github.com/mknudson2/book-project",
+      web: ""
     },
     "F1 Racer Standings": {
       image: "/F1.png",
       description:
         "A web app for checking F1 racer standings within a given season. Uses the Ergast Formula One API and built in JavaScript.",
       link: "https://github.com/mknudson2/week-7-day-4-hw",
+      web: ""
     },
     Norrœna: {
       image: "/Norroena.png",
       description:
         "From my captstone project at Coding Temple, Norrœna is a prototype for a longstanding passion project of mine. Born of my background in academia, this project is aimed at being the ultimate hub for Old Norse/Old Icelandic studies for enthusiasts, novices, and experienced researches alike with interactive texts, a custom dictionary, resources, and unique lessons in Old Norse language. Users can create a profile and connect with other researchers all over the world. Built using React, Vite, TypeScript, Python, Flask, PostgreSQL, with API testing via Insomnia. ",
       link: "https://github.com/mknudson2/capstone-react",
+      web: "https://delightful-moonbeam-2f8522.netlify.app/"
     },
   };
 
@@ -73,11 +81,19 @@ function Dev() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
+          <BsGithub className="gitHubIcon" />
+          {/* <img
             className="gitHubIcon"
             src="/github-icon-1.svg"
             alt="GitHub Icon"
-          />
+          /> */}
+        </a>
+        <a
+          href={selectedProjectData.web}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CgWebsite className="dev-webIcon" />
         </a>
       </p>
     </>
