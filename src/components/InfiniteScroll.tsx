5cf6f281acfd0ea9eb47dd23061eb2cd113a16bc
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-11-04 17:46:26 -0600

    hopefully last scroll speed test

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -33,7 +33,7 @@ function InfiniteScroll() {
 
   return (
     <>
-      <div className="scroller" data-direction="right" scroll-speed="fast">
+      <div className="scroller" data-direction="right" data-speed="fast">
         <ul className="scroll-list scroller_inner">
           <li>
             <img src="/html.png" alt="html5" />
